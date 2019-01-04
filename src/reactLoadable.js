87afa0297a167168398c1ee9999f773512b5d97c
michaeldimmitt <michaelgdimmitt@gmail.com>
2019-01-04 01:18:18 -0500

    remove unused App.

--- src/reactLoadable.js
+++ src/reactLoadable.js
@@ -1,10 +1,6 @@
 import Loadable from 'react-loadable';
 import Loading from './components/Loading';
 
-export const App = Loadable({
-    loader: () => import('./components/App'),
-    loading: Loading,
-});
 export const Login = Loadable({
     loader: () => import('./components/Login'),
     loading: Loading,
