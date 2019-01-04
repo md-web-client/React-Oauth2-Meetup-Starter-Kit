052c8c1691c0d5d844b5a02bded8e3a4f3ae3fcc
michaeldimmitt <michaelgdimmitt@gmail.com>
2019-01-04 01:10:08 -0500

    Show all meetup details.

--- src/components/MeetupDetails.js
+++ src/components/MeetupDetails.js
@@ -1,9 +1,35 @@
 import React from 'react'
 
 const MeetupDetails = ({meetup}) => {
+  console.log({meetup})
+
 
   return <div>
-        {meetup.name} 
+      { meetup.created}
+      <br/>{ meetup.description}
+      <br/>{ meetup.duration}
+      <br/>{ meetup.group.created}
+      <br/>{ meetup.group.name}
+      <br/>{ meetup.group.id}
+      <br/>{ meetup.group.join_mode}
+      <br/>{ meetup.group.lat}
+      <br/>{ meetup.id}
+      <br/>{ meetup.link}
+      <br/>{ meetup.local_date}
+      <br/>{ meetup.local_time}
+      <br/>{ meetup.name}
+      <br/>{ meetup.status}
+      <br/>{ meetup.time}
+      <br/>{ meetup.updated}
+      <br/>{ meetup.utc_offset}
+      <br/>{ meetup.venue.id}
+      <br/>{ meetup.venue.name}
+      <br/>{ meetup.venue.lat}
+      <br/>{ meetup.venue.lon}
+      <br/>{ meetup.venue.repinned}
+      <br/>{ meetup.visibility}
+      <br/>{ meetup.waitlist_count}
+      <br/>{ meetup.yes_rsvp_count}
   </div>
 }
 
